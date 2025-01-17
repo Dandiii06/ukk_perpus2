5466a78cee6056faf64ce1b97d02a9ad9abeda3f
rizki dandi <saputrarizkidandi@gmail.com>
2025-01-17 14:17:20 +0700

    erd db clear

--- app/Models/Buku.php
+++ app/Models/Buku.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use App\Models\KategoriBuku;
+use App\Models\Pinjam;
+use App\Models\ulasan;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Buku extends Model
 {
     use HasFactory;
+    public function kategori_buku(){
+        return $this->belongsTo(KategoriBuku::class);
+    }
+
+    public function pinjams(){
+        return $this->hasMany(Pinjam::class);
+    }
+
+    public function ulasans(){
+        return $this->hasMany(ulasan::class);
+    }
 }
--- app/Models/ulasan.php
+++ app/Models/ulasan.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Buku;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ulasan extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function buku(){
+        return $this->belongsTo(Buku::class);
+    }
 }
